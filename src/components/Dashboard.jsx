2993c133fff100b15e375b95064ee86a9a752126
nahyun park <pnh13584@gmail.com>
2025-02-06 17:51:53 +0900

    Fix: 포켓몬 선택 함수 오류 수정

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,15 +1,17 @@
 import styled from "styled-components";
 
+// 리셋한 p 태그의 스타일 정하기 
 const StdP= styled.p`
     color: red;
 `;
 
+// 컨테이너의 스타일 정하기 
 const StdContainer = styled.div`
     display: flex;
     gap: 10px;
 
 `
-
+// 컨테이너 안 박스 포켓몬 스타일 정하기 
 const StdBox= styled.div`
     width: 100px;
     height: 100px;
@@ -22,6 +24,7 @@ const Dashboard = () => {
     return ( 
         <>
         <StdP>나만의 포켓몬</StdP>
+        {/* 클릭한 포켓몬을 가져오는 컨테이너 */}
         <StdContainer>
             <StdBox><img src="https://i.imgur.com/sJ5KcxJ.png" width="100"alt="..."/>
             </StdBox>
--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,28 +1,30 @@
 import styled from "styled-components";
 
+// 버튼 스타일 정하기 
 const StdBtn = styled.button`
         background-color: red;
         color: white;
     `
 
+// 포켓몬 카드 스타일 정하기
 const StyledCard = styled.div`
     margin-top: 6px;
     border-radius: 10px;
     box-shadow: 5px 5px 10px grey;
     background-color: white;
     background-position: center;
-    overflow: hidden;
+    flex: 0 0 14%;
 `
-
-const PokemonCard = ({data}) => {
+// 포켓몬 카드 형태 정하는 함수 
+const PokemonCard = ({data, onClick}) => {
 
     return ( 
         <>
         <StyledCard>
-                <div><image src={data.img_url}/></div>
+                <div><img src={data.img_url}/></div>
                 <div>{data.korean_name}</div>
-                <div>{data.id}</div>
-                <StdBtn>추가</StdBtn>
+                <div>No.{data.id}</div>
+                <StdBtn onClick={onClick}>추가</StdBtn>
         </StyledCard>    
         </>
      );
