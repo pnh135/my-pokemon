ecc6d0e1bc94cd31c0aa693c64e639f8c0a1bd19
nahyun park <pnh13584@gmail.com>
2025-02-09 23:45:45 +0900

    Feat: 삭제 함수 추가

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,6 +1,5 @@
 import { react, useState } from "react";
 import styled from "styled-components";
-import PokemonBall from "./PokemonBall";
 
 // 리셋한 p 태그의 스타일 정하기
 const StdP = styled.p`
@@ -11,65 +10,35 @@ const StdP = styled.p`
 const StdContainer = styled.div`
   display: flex;
   gap: 10px;
-`;
-// 컨테이너 안 박스 포켓몬 스타일 정하기
-const StdBox = styled.div`
-  width: 100px;
-  height: 100px;
-  border: 5px dotted gray;
-  background-color: white;
-  margin: 20px;
+  justify-content: center;
+  align-content: space-evenly;
 `;
 
-const Dashboard = ({ ball }) => {
+const Dashboard = ({ ball, setBall, GetEmptyBalls }) => {
+  // 포켓몬 삭제 기능
+  const removePokemon = (id) => {
+    const updateMyPokemon = ball.filter((item) => {
+      return item.id !== id;
+    });
+    setBall(updateMyPokemon);
+  };
   return (
     <>
       <StdP>나만의 포켓몬</StdP>
       {/* 클릭한 포켓몬을 가져오는 컨테이너 */}
       <StdContainer>
-        {console.log(ball)}
-        <StdBox>
-          {{ ball }[0] !== undefined ? (
-            <div>{ball[0].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
-        <StdBox>
-          {{ ball }[1] !== undefined ? (
-            <div>{ball[1].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
-        <StdBox>
-          {{ ball }[2] !== undefined ? (
-            <div>{ball[2].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
-        <StdBox>
-          {{ ball }[3] !== undefined ? (
-            <div>{ball[3].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
-        <StdBox>
-          {{ ball }[4] !== undefined ? (
-            <div>{ball[4].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
-        <StdBox>
-          {{ ball }[5] !== undefined ? (
-            <div>{ball[5].id}</div>
-          ) : (
-            <PokemonBall />
-          )}
-        </StdBox>
+        {ball.map((item) => (
+          <StdBox key={item.id}>
+            <div>
+              <img src={item.img_url} />
+            </div>
+            <div>{item.korean_name}</div>
+            <div>No.{item.id}</div>
+            <button onClick={() => removePokemon(item.id)}>제거</button>
+          </StdBox>
+        ))}
+
+        {GetEmptyBalls()}
       </StdContainer>
     </>
   );
--- src/pages/Dex.jsx
+++ src/pages/Dex.jsx
@@ -1,11 +1,38 @@
 import { react, useState } from "react";
 
+import styled from "styled-components";
 import Dashboard from "../components/Dashboard";
 import PokemonList from "../components/PokemonList";
+import PokemonBall from "../components/PokemonBall";
+
+// 컨테이너 안 박스 포켓볼 스타일 정하기
+const StdBox = styled.div`
+  width: 100px;
+  height: 100px;
+  border: 5px dotted gray;
+  background-color: white;
+  margin: 20px;
+`;
 
 const Dex = () => {
   // 선택한 포켓몬을 새 배열로 작성
   const [ball, setBall] = useState([]);
+
+  // 선택한 포켓몬이 없을 때 이미지가 보이도록 하는 함수
+  const GetEmptyBalls = () => {
+    const EmptyBall = [];
+    const MyBalls = 6 - ball.length;
+
+    for (let i = 0; i < MyBalls; i++) {
+      EmptyBall.push(
+        <StdBox key={i}>
+          <PokemonBall />
+        </StdBox>
+      );
+    }
+    return EmptyBall;
+  };
+
   // 포켓몬을 선택하는 함수
   const AddPokemon = (item) => {
     if (ball.length < 6) {
@@ -27,7 +54,11 @@ const Dex = () => {
   return (
     <>
       <section>
-        <Dashboard ball={ball} AddPokemon={AddPokemon} />
+        <Dashboard
+          ball={ball}
+          setBall={setBall}
+          GetEmptyBalls={GetEmptyBalls}
+        />
       </section>
       <section>
         <PokemonList AddPokemon={AddPokemon} />
