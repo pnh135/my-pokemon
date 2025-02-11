04fe80cafdde72163d1e6ce78e2bad451796150d
nahyun park <pnh13584@gmail.com>
2025-02-11 22:58:49 +0900

    Fix: addpokemon 함수 오류 수정

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -2,32 +2,51 @@ import { useContext } from "react";
 import Button from "../styles/Button";
 import Card from "../styles/Card";
 import { BallContext } from "../context/ContextProvider";
+import { useNavigate } from "react-router-dom";
 
 // 포켓몬 카드 형태 정하는 함수
 const PokemonCard = ({ data }) => {
+  const navigate = useNavigate();
   const { ball, setBall } = useContext(BallContext);
-  console.log(ball);
-  console.log(data);
 
-  const AddPokemon = (item) => {
-    // 배열의 길이(선택된 포켓몬)이 6개가 되지 않을 때 추가
-    if (ball.length < 6) {
-      setBall([...ball, item]);
-    } else {
-      alert("포켓몬은 6마리가 최대입니다.");
+  const addPokemon = (pokemon) => {
+    // 최대 6개까지만 선택 가능하도록 제한
+    if (ball.length > 5) {
+      return alert("최대 6개의 포켓몬만 선택 할 수 있습니다.");
     }
-    // setBall([...ball, item]);
+    // 이미 선택된 포켓몬인지 확인
+    if (ball.some((item) => item.id === pokemon.id)) {
+      alert("해당 포켓몬이 이미 존재합니다.");
+    } else {
+      // 선택되지 않은 포켓몬이면 myPokemon 배열에 추가
+      setBall([...ball, pokemon]);
+    }
   };
 
   return (
     <>
       <Card>
         <div>
-          <img src={data.img_url} />
+          {" "}
+          <div
+            onClick={() =>
+              navigate(`/Details/?id=${data.id}`, {
+                state: {
+                  img_url: `${data.img_url}`,
+                  korean_name: `${data.korean_name}`,
+                  types: `${data.types}`,
+                  description: `${data.description}`,
+                },
+              })
+            }
+          >
+            <img src={data.img_url} />
+          </div>
+          <div className="card-name">{data.korean_name}</div>
+          <div className="card-num">No.{data.id}</div>
         </div>
-        <div className="card-name">{data.korean_name}</div>
-        <div className="card-num">No.{data.id}</div>
-        <Button onClick={AddPokemon(data)}>추가</Button>
+
+        <Button onClick={() => addPokemon(data)}>추가</Button>
       </Card>
     </>
   );
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -1,7 +1,6 @@
 import PokemonCard from "./PokemonCard";
 import MOCK_DATA from "../data/MOCK_DATA";
 import styled from "styled-components";
-import { useNavigate } from "react-router-dom";
 
 const Container = styled.div`
   margin-top: 5%;
@@ -15,26 +14,13 @@ const Container = styled.div`
 `;
 
 const PokemonList = () => {
-  const navigate = useNavigate();
   return (
     <>
       {/* 받은 포켓몬 카드 함수로 map 작성해서 리스트 만들기 */}
       <Container>
         {MOCK_DATA.map((data) => (
           //  카드를 누르면 디테일 페이지로 가도록 navigate 처리
-          <div
-            key={data.id}
-            onClick={() => {
-              navigate(`/Details/?id=${data.id}`, {
-                state: {
-                  img_url: `${data.img_url}`,
-                  korean_name: `${data.korean_name}`,
-                  types: `${data.types}`,
-                  description: `${data.description}`,
-                },
-              });
-            }}
-          >
+          <div key={data.id}>
             <PokemonCard key={data.id} data={data} />
           </div>
         ))}
