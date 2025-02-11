bd7608faa8ddbc18eec02146f01f076a085c65da
nahyun park <pnh13584@gmail.com>
2025-02-11 22:27:51 +0900

    .

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -6,15 +6,17 @@ import { BallContext } from "../context/ContextProvider";
 // 포켓몬 카드 형태 정하는 함수
 const PokemonCard = ({ data }) => {
   const { ball, setBall } = useContext(BallContext);
+  console.log(ball);
+  console.log(data);
 
   const AddPokemon = (item) => {
     // 배열의 길이(선택된 포켓몬)이 6개가 되지 않을 때 추가
     if (ball.length < 6) {
-      ball.push(item);
+      setBall([...ball, item]);
     } else {
       alert("포켓몬은 6마리가 최대입니다.");
     }
-    setBall([...ball, item]);
+    // setBall([...ball, item]);
   };
 
   return (
@@ -25,7 +27,7 @@ const PokemonCard = ({ data }) => {
         </div>
         <div className="card-name">{data.korean_name}</div>
         <div className="card-num">No.{data.id}</div>
-        <Button onClick={AddPokemon}>추가</Button>
+        <Button onClick={AddPokemon(data)}>추가</Button>
       </Card>
     </>
   );
